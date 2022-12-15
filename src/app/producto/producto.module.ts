620a1366daae739103def7b2d95189e93ffe6e04
Barboza Ordóñez <bbarbozao@minsait.com>
2022-12-15 18:24:56 +0100

    first commit

---
+++ src/app/producto/producto.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductoDetailComponent } from './producto-detail/producto-detail.component';
+import { ProductoFormComponent } from './producto-form/producto-form.component';
+import { ProductoMainComponent } from './producto-main/producto-main.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    ProductoDetailComponent,
+    ProductoFormComponent,
+    ProductoMainComponent
+  ],
+  exports:[
+    ProductoDetailComponent,
+    ProductoFormComponent,
+    ProductoMainComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ]
+})
+export class ProductoModule { }
